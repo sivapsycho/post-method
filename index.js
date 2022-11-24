2a98ddf118c67bda88a6648f8122247cdcf16895
sivaphyco <narayanakanaparthi9@gmail.com>
2022-11-24 17:09:12 +0530

    post

---
+++ index.js
@@ -0,0 +1,24 @@
+const express=require("express")
+const app=express()
+const cors=require("cors")
+const bodyParser=require("body-parser")
+const port=3000;
+app.use(bodyParser.urlencoded({
+	extended:true
+}))
+app.use(bodyParser.json())
+
+app.use(cors())
+
+const student={rollno:44,cls:"bcom",name:"kamesh mani"}
+
+app.get("/",(req,res)=>{
+	res.send(student)
+})
+
+app.post("/newData",(req,res)=>{
+	const {username,password}=req.body
+	console.log(req.body)
+	res.send("respond send sucessfully")
+})
+app.listen(port,()=>console.log("server is running on port"))
